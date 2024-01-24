15b7b7e94210fcab5946fd8b607a791641d304a2
Yue Moua <ymoua27@gmail.com>
2024-01-24 14:46:25 -0800

    grammar

--- ch12.py
+++ ch12.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Script:                       Create to prompt user choice between TCP Port Range Scanner mode and ICMP Ping Sweep mode
+# Script:                       Create a script to prompt user choice between TCP Port Range Scanner mode and ICMP Ping Sweep mode
 # Author:                       Yue Moua
 # Date of latest revision:      1/23/2024
 # Purpose:                      Challenge 12
---
+++ ch13.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Script:                       Create to prompt user choice between TCP Port Range Scanner mode and ICMP Ping Sweep mode
+# Author:                       Yue Moua
+# Date of latest revision:      1/23/2024
+# Purpose:                      Challenge 13
